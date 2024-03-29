247e19a41e99bf42b0b5cc4aa492cdba7cd010a6
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-03-29 11:32:48 +0100

    Feat -> component: add case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { Case } from "../../components";
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (
@@ -9,7 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main>
+      <main className="container">
+        <h1 className="title">Blackjack Analyzer</h1>
+
+        <Case icon={faFile} title="Total Games" value="2935" />
       </main>
     </>
   );
